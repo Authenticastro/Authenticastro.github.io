28bca70a411b7eddfff18a255b063860bb5ec495
sai2698 <pakkalaabhiram10@gmail.com>
2024-08-06 20:40:26 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,418 @@
+/**
+* Template Name: eNno
+* Template URL: https://bootstrapmade.com/enno-free-simple-bootstrap-template/
+* Updated: Mar 17 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
+
+
+
+
+
+
+
+
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+var starDensity = .216;
+var speedCoeff = .05;
+var width;
+var height;
+var starCount;
+var circleRadius;
+var circleCenter;
+var first = true;
+var giantColor = '180,184,240';
+var starColor = '226,225,142';
+var cometColor = '226,225,224';
+var canva = document.getElementById('universe');
+var stars = [];
+
+windowResizeHandler();
+window.addEventListener('resize', windowResizeHandler, false);
+
+createUniverse();
+
+function createUniverse() {
+  universe = canva.getContext('2d');
+
+  for (var i = 0; i < starCount; i++) {
+    stars[i] = new Star();
+    stars[i].reset();
+  }
+
+  draw();
+}
+
+function draw() {
+  universe.clearRect(0, 0, width, height);
+
+  var starsLength = stars.length;
+
+  for (var i = 0; i < starsLength; i++) {
+    var star = stars[i];
+    star.move();
+    star.fadeIn();
+    star.fadeOut();
+    star.draw();
+  }
+
+  window.requestAnimationFrame(draw);
+}
+
+function Star() {
+
+  this.reset = function() {
+    this.giant = getProbability(3);
+    this.comet = this.giant || first ? false : getProbability(10);
+    this.x = getRandInterval(0, width - 10);
+    this.y = getRandInterval(0, height);
+    this.r = getRandInterval(1.1, 2.6);
+    this.dx = getRandInterval(speedCoeff, 6 * speedCoeff) + (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120) + speedCoeff * 2;
+    this.dy = -getRandInterval(speedCoeff, 6 * speedCoeff) - (this.comet + 1 - 1) * speedCoeff * getRandInterval(50, 120);
+    this.fadingOut = null;
+    this.fadingIn = true;
+    this.opacity = 0;
+    this.opacityTresh = getRandInterval(.2, 1 - (this.comet + 1 - 1) * .4);
+    this.do = getRandInterval(0.0005, 0.002) + (this.comet + 1 - 1) * .001;
+  };
+
+  this.fadeIn = function() {
+    if (this.fadingIn) {
+      this.fadingIn = this.opacity > this.opacityTresh ? false : true;
+      this.opacity += this.do;
+    }
+  };
+
+  this.fadeOut = function() {
+    if (this.fadingOut) {
+      this.fadingOut = this.opacity < 0 ? false : true;
+      this.opacity -= this.do / 2;
+      if (this.x > width || this.y < 0) {
+        this.fadingOut = false;
+        this.reset();
+      }
+    }
+  };
+
+  this.draw = function() {
+    universe.beginPath();
+
+    if (this.giant) {
+      universe.fillStyle = 'rgba(' + giantColor + ',' + this.opacity + ')';
+      universe.arc(this.x, this.y, 2, 0, 2 * Math.PI, false);
+    } else if (this.comet) {
+      universe.fillStyle = 'rgba(' + cometColor + ',' + this.opacity + ')';
+      universe.arc(this.x, this.y, 1.5, 0, 2 * Math.PI, false);
+
+      //comet tail
+      for (var i = 0; i < 30; i++) {
+        universe.fillStyle = 'rgba(' + cometColor + ',' + (this.opacity - (this.opacity / 20) * i) + ')';
+        universe.rect(this.x - this.dx / 4 * i, this.y - this.dy / 4 * i - 2, 2, 2);
+        universe.fill();
+      }
+    } else {
+      universe.fillStyle = 'rgba(' + starColor + ',' + this.opacity + ')';
+      universe.rect(this.x, this.y, this.r, this.r);
+    }
+
+    universe.closePath();
+    universe.fill();
+  };
+
+  this.move = function() {
+    this.x += this.dx;
+    this.y += this.dy;
+    if (this.fadingOut === false) {
+      this.reset();
+    }
+    if (this.x > width - (width / 4) || this.y < 0) {
+      this.fadingOut = true;
+    }
+  };
+
+  (function() {
+    setTimeout(function() {
+      first = false;
+    }, 50)
+  })()
+}
+
+function getProbability(percents) {
+  return ((Math.floor(Math.random() * 1000) + 1) < percents * 10);
+}
+
+function getRandInterval(min, max) {
+  return (Math.random() * (max - min) + min);
+}
+
+function windowResizeHandler() {
+  width = window.innerWidth;
+  height = window.innerHeight;
+  starCount = width * starDensity;
+  circleRadius = (width > height ? height / 2 : width / 2);
+  circleCenter = {
+    x: width / 2,
+    y: height / 2
+  }
+
+  canva.setAttribute('width', width);
+  canva.setAttribute('height', height*0.75);
+}
+
+
+
+
+
+
